25c8524d879ffc47d3b238e5cef0183b9bb0f127
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-21 10:16:28 +0100

    create db function written

--- server.js
+++ server.js
@@ -6,6 +6,34 @@ server.use(parser.json());
 server.use(express.static('client/build'));
 server.use(parser.urlencoded({extended: true}));
 
+
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+
+MOngoClient.connect("mongodb://localhost:27017", function(err, client){
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("bucket_list");
+  console.log("connected to database");
+
+  server.post('/api/countries', function(req, res, next){
+    const countryCollection = db.collection("countries");
+    const countryToSave = req.body;
+    countryCollection.save(countryToSave, function(err, result){
+      if(err) next(err);
+      res.status(201);
+      // res.json(result.ops[0])
+      console.log("saved to DB");
+
+    })
+  })
+
+  
+});
+
 server.listen(3000, function(){
   console.log("Listening on port 3000");
 });
